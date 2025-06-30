584ae083fe4e47d381d36e51570427cceeecb783
behrad <malakoutibehrad@gmail.com>
2025-06-30 17:06:06 +0330

    complete register api

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import UsernameList
+from .views import UsernameList, Register
 
 urlpatterns = [
+    path('register/', Register.as_view()),
     path('', UsernameList.as_view()),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -11,3 +11,14 @@ from .serializers import UserSerializer
 class UsernameList(APIView):
     def get(self, request, *args, **kwargs):
         return Response(User.objects.values_list('username', flat=True))
+
+class Register(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(user.password)
+            user.save()
+            return Response(f"User with {user.username=} and {user.phone=} has been created successfully!", status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
