835661f107e380d4b1291f0c71b5c2adaf0a55bb
behrad <malakoutibehrad@gmail.com>
2025-07-02 20:58:05 +0330

    Add user field to workout model

--- workout/models.py
+++ workout/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from authentication.models import User
 
 CHOICES = [(i, i) for i in range(1, 6)]
 
 class Workout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name_english = models.CharField(max_length=30)
     name_persian = models.CharField(max_length=30, blank=True)
     score = models.SmallIntegerField(choices=CHOICES, blank=True, null=True)
     user_tips = models.TextField(blank=True)
     sets = models.JSONField(default=list, blank=True, null=True)
+    
 
 
