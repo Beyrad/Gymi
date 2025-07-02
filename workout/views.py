8a6770abfad78d2590279994fd5c28cdc3d860f0
behrad <malakoutibehrad@gmail.com>
2025-07-02 21:28:59 +0330

    Create PUT api

--- workout/views.py
+++ workout/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+from .models import Workout
+
+class CreateUpdateWorkout(APIView):
+    def put(self, request, id=None):
+        if id:
+            workout = get_object_or_404(Workout, id=id)
+            serializer = WorkoutSerializer(instance=workout, data=request.data)
+            
