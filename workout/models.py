858ab74e4182f481107c1441d4129c3b486d33e3
behrad <malakoutibehrad@gmail.com>
2025-07-15 22:56:15 +0330

    Add first llm api integration codes

--- workout/models.py
+++ workout/models.py
@@ -1,7 +1,30 @@
 from django.db import models
 from authentication.models import User
+from openai import OpenAI
+from decouple import config
 
 CHOICES = [(i, i) for i in range(1, 6)]
+
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=config('API_KEY'),
+)
+
+def Ask(prompt):
+    completion = client.chat.completions.create(
+        extra_headers={},
+        extra_body={},
+        model="deepseek/deepseek-r1:free",
+        messages=[
+            { "role": "user", "content": prompt },
+            { "role": "system", "content": "consider yourself as a professional gym trainer that gives" \
+                                            "instructions that help user build strong muscles with good habits and with safety through the workout" }
+       ]
+    )
+    return completion.choices[0].message.content
+
+
+
 
 class Workout(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,6 +33,11 @@ class Workout(models.Model):
     score = models.SmallIntegerField(choices=CHOICES, blank=True, null=True)
     user_tips = models.TextField(blank=True)
     sets = models.JSONField(default=list, blank=True, null=True)
+    how_to_do = models.TextField(blank=True)
+
+    def AskHowToDo(self):
+        res = Ask(f"How to do this workout called {self.name_english} demonstrate the steps clearly and say the safety tips")
+
     
 
 
